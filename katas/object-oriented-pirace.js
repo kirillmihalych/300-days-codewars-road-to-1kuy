093a7dc8e3b49351ffc62319d172d7a9530d6224
kirillmihalych <tiltlessman@gmail.com>
2024-02-19 21:16:53 +0300

    [19 day][8 kuy] object oriented piracy

---
+++ katas/object-oriented-pirace.js
@@ -0,0 +1,11 @@
+class Ship {
+  constructor(draft, crew) {
+    this.draft = draft
+    this.crew = crew
+  }
+
+  //YOUR CODE HERE...
+  isWorthIt() {
+    return this.draft - this.crew * 1.5 > 20
+  }
+}
